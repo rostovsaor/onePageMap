df4fcb2afa16c7e759531b58afa3f6f492d14939
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2021-11-19 03:19:05 +0200

    adicionar pagina loja duolingo

---
+++ src/components/maza/StoreCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container q-mb-lg flex justify-between no-wrap">
+    <div class="">
+      <q-icon class="q-ma-md" name="img:img/ofensiva1.png" size="6rem"/>
+    </div>
+    <div>
+      <h6 class="q-my-sm text-grey-9">{{this.name}}</h6>
+      <p class="text-grey-8 text-body1">{{this.text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoreCard',
+  props:{
+    name: String,
+    value: Number,
+    text: String,
+  }
+    // <StoreCard name="Bloqueio de ofensiva" value="200" text="Proteja a sua ofensiva se perder um dia de prática. Coloque até 2 em uso de umma vez."/>
+
+}
+</script>
+
+<style scoped>
+  .container {
+    border-style: solid;
+    border-color: rgb(224, 224, 224);
+    border-width: 0.18rem;
+    border-radius: 1.4em;
+  }
+  /* .title {
+    font-weight: 800;
+  } */
+  h6 {
+    font-weight: 800;
+  }
+</style>
---
+++ src/pages/Maza.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="q-ma-lg">
+    <h5>Ofensiva</h5>
+    <StoreCard name="Bloqueio de ofensiva" value="200" text="Proteja a sua ofensiva se perder um dia de prática. Coloque até 2 em uso de umma vez." icon="img/"/>
+    <StoreCard name="Aposta: O Dobro ou Nada" value="50" text="Mantenha 7 dias de ofensiva pra dobrar a sua aposta de 50 cristais"/>
+
+    <h5>Cristais</h5>
+    <StoreCard name="Bloqueio de ofensiva" value="200" text="Proteja a sua ofensiva se perder um dia de prática. Coloque até 2 em uso de umma vez."/>
+    <StoreCard name="Aposta: O Dobro ou Nada" value="50" text="Mantenha 7 dias de ofensiva pra dobrar a sua aposta de 50 cristais"/>
+
+    <h5>Vidas</h5>
+    <StoreCard name="Bloqueio de ofensiva" value="200" text="Proteja a sua ofensiva se perder um dia de prática. Coloque até 2 em uso de umma vez."/>
+    <StoreCard name="Aposta: O Dobro ou Nada" value="50" text="Mantenha 7 dias de ofensiva pra dobrar a sua aposta de 50 cristais"/>
+
+    <h5>Trajes</h5>
+    <StoreCard name="Bloqueio de ofensiva" value="200" text="Proteja a sua ofensiva se perder um dia de prática. Coloque até 2 em uso de umma vez."/>
+    <StoreCard name="Aposta: O Dobro ou Nada" value="50" text="Mantenha 7 dias de ofensiva pra dobrar a sua aposta de 50 cristais"/>
+  </div>
+</template>
+
+<script>
+    // import BigNewsComponent from "../items-component/BigNewsComponent";
+
+import StoreCard from "components/maza/StoreCard.vue"
+export default {
+  name: 'Maza',
+  components: {
+    StoreCard
+  }
+}
+</script>
+
+<style>
+  h4, h5 {
+    font-weight: 800;
+    color:rgb(80, 80, 80);
+    margin: 3rem 0 1rem 0;
+
+  }
+</style>
